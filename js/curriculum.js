84c22c36f0d4edaab770abd0c27a9c0cf22eedc3
petarmarkota <petar.markota@gmail.com>
2023-04-26 17:34:43 +0200

    fixed curriculum not showing

--- js/curriculum.js
+++ js/curriculum.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
   var token = sessionStorage.getItem("token");
+  var username = sessionStorage.getItem("username");
+
+  // Store token and username in cookies
+  document.cookie =
+    "token=" + token + "; expires=Thu, 1 Jan 2099 12:00:00 UTC; path=/;";
+  document.cookie =
+    "username=" + username + "; expires=Thu, 1 Jan 2099 12:00:00 UTC; path=/;";
   console.log(token);
 
   $.ajax({
